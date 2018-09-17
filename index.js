c999d52457b217825b58a700018c82ed1cb86bfe
Maël Nison <mael@fb.com>
2018-09-17 20:49:31 +0100

    Fixes the async entry point

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {readFileSync} = require(`fs`);
 
 const setupModule = require(`./lib/text-layout.js`);
-const binaryData = readFileSync(`${__dirname}/text-layout.wasm`);
+const binaryData = readFileSync(`${__dirname}/lib/text-layout.wasm`);
 
 module.exports = new Promise((resolve, reject) => {
   setupModule(binaryData).then(Module => {
